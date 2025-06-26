6aea87559d8c2ec268c581829442c9f11b62753f
Carlos Izquierdo Marquez <izquierdomarquez03@gmail.com>
2025-06-25 21:19:07 -0600

    Se actualiza la app y los privilegios de los usuarios

--- task/admin.py
+++ task/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from .models import Alumno, Personal, Equipo, Asignacion
 
+class SoloLecturaAdmin(admin.ModelAdmin):
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
 # Register your models here.
 
-admin.site.register(Alumno)
-admin.site.register(Personal)
-admin.site.register(Equipo)
-admin.site.register(Asignacion)
+# Registra cada modelo con el admin personalizado
+admin.site.register(Alumno, SoloLecturaAdmin)
+admin.site.register(Personal, SoloLecturaAdmin)
+admin.site.register(Equipo, SoloLecturaAdmin)
+admin.site.register(Asignacion, SoloLecturaAdmin)
